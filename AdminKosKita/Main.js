b2c5d0a40e0a6f577234f47f246f1da5b97fd29b
Ramadhan Alfarisi <ramadhan_salman_26rpl@student.smktelkom-mlg.sch.id>
2019-08-29 13:33:00 +0700

    Added AdminKosKita

--- AdminKosKita/Main.js
+++ AdminKosKita/Main.js
@@ -1,5 +1,5 @@
 //import liraries
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
@@ -7,17 +7,26 @@ import {
   TouchableOpacity,
   Dimensions,
   Image,
+  Alert
 } from 'react-native';
 import home from '../gambar/home.png';
+import { fbs } from './Config';
 
 // create a component
-const {width: WIDTH} = Dimensions.get('window');
+const { width: WIDTH } = Dimensions.get('window');
 class Main extends Component {
+
+  signout() {
+    Alert.alert('Sign Out', 'Apakah anda yakin untuk sign out ?', [
+      { text: 'Iya', onPress: () => { fbs.auth.signOut() } }, { text: 'Tidak' }
+    ])
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={{marginBottom: 50, alignItems: 'center'}}>
-          <Image source={home} style={{height: 100, width: 100}} />
+        <View style={{ marginBottom: 50, alignItems: 'center' }}>
+          <Image source={home} style={{ height: 100, width: 100 }} />
           <Text
             style={{
               color: '#fff',
@@ -39,7 +48,7 @@ class Main extends Component {
             backgroundColor: '#fff',
             borderRadius: 5,
           }}>
-          <Text style={{color: '#e74c3c', fontSize: 18, fontWeight: '500'}}>
+          <Text style={{ color: '#e74c3c', fontSize: 18, fontWeight: '500' }}>
             Kost
           </Text>
         </TouchableOpacity>
@@ -51,11 +60,27 @@ class Main extends Component {
             alignItems: 'center',
             justifyContent: 'center',
             marginTop: 10,
+            marginBottom: 10,
             backgroundColor: '#fff',
             borderRadius: 5,
           }}>
-          <Text style={{color: '#e74c3c', fontSize: 18, fontWeight: '500'}}>
+          <Text style={{ color: '#e74c3c', fontSize: 18, fontWeight: '500' }}>
             Chat
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.signout()}
+          style={{
+            width: WIDTH - 50,
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 10,
+            backgroundColor: '#fff',
+            borderRadius: 5,
+          }}>
+          <Text style={{ color: '#e74c3c', fontSize: 18, fontWeight: '500' }}>
+            Sign Out
           </Text>
         </TouchableOpacity>
       </View>
